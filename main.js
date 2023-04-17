17be2fdc338873e611b056b29476478fc4b1b321
MJ-rocket <127004932+MJ-rocket@users.noreply.github.com>
2023-04-17 20:05:05 +1000

    JS: 3단계 필터완성

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 // Item filter
 const workBtnContainer = document.querySelector('.side__menu');
 const workBtnContainer2 = document.querySelector('.main__menu');
+const workBtnContainer3 = document.querySelector('.second__menu');
 const mainBtnContainer = document.querySelector('.product__category');
 const productContainer = document.querySelector('.main__top-level');
 const products = document.querySelectorAll('.product');
@@ -22,6 +23,22 @@ workBtnContainer.addEventListener('click', (e) => {
 });
 
 workBtnContainer2.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter
+    if (filter == null) {
+        return;
+    }
+    products.forEach((product) => {
+        console.log(product.dataset.type);
+        if (filter === product.dataset.type) {
+            product.classList.remove('invisible');
+        } else {
+            product.classList.add('invisible');
+        }
+    });
+});
+
+
+workBtnContainer3.addEventListener('click', (e) => {
     const filter = e.target.dataset.filter
     if (filter == null) {
         return;
